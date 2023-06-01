81a6326471638c8ac424ed42d3f971c02e4ed318
765615945 <765615945@users.noreply.github.com>
2023-06-01 16:26:40 +0000

    自动拉取图鉴网数据并更新规则

---
+++ rule.hpp
@@ -0,0 +1,33 @@
+// 贤客楼 王老板 一 金币 周1357
+
+// 本代码由 ./assets/generateRule.py 自动生成
+#ifndef RULE_HPP
+#define RULE_HPP
+#include "Calculator.hpp"
+#include "functions.hpp"
+
+/**
+ * @brief
+ *
+ * @param rule
+ * @param s
+ * @return int : the optimal `full` value
+ */
+int banquetRule(BanquetRule *const &rule, States &s) {
+    BanquetStrictRule
+        *strictRule[NUM_CHEFS *
+                    DISH_PER_CHEF]; // 下一阶段的规则，不受意图生效次数的影响
+    BanquetLenientRule *lenientRule[NUM_CHEFS * DISH_PER_CHEF];
+    for (int i = 0; i < NUM_CHEFS * DISH_PER_CHEF; i++) {
+        strictRule[i] = &rule[i].strictRule;
+        lenientRule[i] = &rule[i].lenientRule;
+    }
+
+// 第1轮
+return 3;
+
+    
+}
+
+#endif
+
